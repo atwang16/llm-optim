169467e7a39eb52e33b67d3e4db5ce3e35e6e75d
diliash <iliashdenys@gmail.com>
2024-11-27 12:13:54 -0800

    Parallel processing

---
+++ kernel_inference_parallel.py
@@ -0,0 +1,195 @@
+import argparse
+import multiprocessing as mp
+import os
+import pickle
+from functools import partial
+from glob import glob
+
+import numpy as np
+import torch
+from llmoptim.hierarchy_pdf import HierarchyCache, HierarchyPDF
+from llmoptim.kernel import fill_rows
+from llmoptim.tokenizer import Tokenizer
+from llmoptim.utils import int_to_list_int, str_seq_to_int
+from models.llama import Llama
+from tqdm import tqdm
+
+
+def load_ckpts_into_seq(ckpts_path):
+    # Loads checkpoints and constructs sequence for each parameter
+    # Returns a dict {"{param_name}": np.ndarray({n_ckpts}), ...},
+    # where n_ckpts practically is time series length we provide as an input
+    # remember to account for param_name being actually {layer_name}_{param_flattened_index}
+    model_state_dicts = [torch.load(ckpt_path) for ckpt_path in sorted(glob(f"{ckpts_path}/*"))]
+    param_specs = [(param_name, param_mat.size()) for param_name, param_mat in model_state_dicts[0].items()]
+    sequences = {}
+    for param_spec in param_specs:
+        param_name, param_size = param_spec
+        total_flattened = np.prod(param_size)
+        for i in range(total_flattened):
+            param_seq = np.zeros(len(model_state_dicts))
+            for j, model_state_dict in enumerate(model_state_dicts):
+                param_seq[j] = model_state_dict[param_name].flatten()[i]
+            sequences[f"{param_name}_{i}"] = param_seq
+    return sequences
+
+
+def get_pdf(
+    sequence: np.ndarray,
+    llama: Llama,
+    good_tokens: str,
+    output_file: str = None,
+    refinement_depth: int = 1,
+    mode: str = "neighbor",
+    str_delimiter: str = ",",
+) -> list[HierarchyPDF]:
+    """Compute hierarchical PDF for each step in sequence.
+
+    :param sequence: np.ndarray of integer sequence states between 0 and 10^k (in practice, should be scaled to
+    roughly 150 to 850).
+    :param llama: LLM model for predicting sequences
+    :param good_tokens: IDs of tokens to get probabilities for ("0123456789")
+    :param output_file: path to output file for PDFs, defaults to None
+    :param refinement_depth: depth of refinement for hierarchical PDF (for coarse bins), defaults to 1 (only top-level
+    bin)
+    :param mode: "neighbor" or "all" for refining coarse bins around main prediction, defaults to "neighbor"
+    :param str_delimiter: delimiter for string version of sequence, defaults to ","
+    :return: list of hierarchical PDFs for each step in sequence
+    """
+    # Get HierarchyPDF
+    # If output_dir is not None, save probs into npy (this way we can split jobs for even more distributed computing)
+    # build sequence string
+    sequence_str = llama.tokenizer.to_string(sequence)
+    delimiters = [i for i, char in enumerate(sequence_str) if char == str_delimiter]
+
+    probs, kv_cache, _ = llama.forward_probs(sequence_str, good_tokens, use_cache=True)
+    start_idx = 0
+    pdf_list = []
+    for num_list, delim_idx in tqdm(zip(str_seq_to_int(sequence_str), delimiters), total=len(sequence)):
+        pdf = HierarchyPDF.from_sample(num_list, probs[0, start_idx:delim_idx])
+        rel_idx_from_end = delim_idx - len(sequence_str) - 1
+        kv_cache_trimmed = kv_cache.trim(rel_idx_from_end)
+
+        pdf.refine(
+            llama,
+            s_traj=sequence_str[:delim_idx],
+            refinement_depth=refinement_depth,
+            kv_cache=kv_cache_trimmed,
+            good_tokens=good_tokens,
+            mode=mode,
+        )
+        pdf_list.append(pdf)
+
+        start_idx = delim_idx + 1
+
+    if output_file is not None:
+        with open(output_file, "wb") as output_file:
+            pickle.dump({"pdf": pdf_list}, output_file)
+
+    return pdf_list
+
+
+def get_kernel(pdfs: list[HierarchyPDF], sequence: np.ndarray, init_min: float, init_max: float, output_file: str = None):
+    """Compute transition kernel from hierarchical PDFs for each parameter
+
+    :param pdfs: list of HierarchyPDF objects representing P^{(i,i)}(X_{t+1}|X_t = sequence[t])
+    :param sequence: np.ndarray of integer sequence states between 0 and 10^k (in practice, should be scaled to
+    roughly 150 to 850).
+    :param output_dir: _description_, defaults to None
+    :return: _description_
+    """
+    # FIXME: may have an off by 1 error in aligning PDFs to the sequence?
+    assert len(pdfs) == sequence.shape[0]
+
+    # Get kernel
+    pdf_unrolled = []
+    for pdf in pdfs:
+        pdf_unrolled.append(pdf.get_prob())  # (10^prec,)
+    pdf_unrolled = np.array(pdf_unrolled)  # (len(sequence), 10^prec)
+    assert pdf_unrolled.shape[0] == sequence.shape[0]
+
+    # construct sparse probability matrix
+    sparse_prob = np.zeros((pdf_unrolled.shape[1], pdf_unrolled.shape[1]))
+    sparse_prob[sequence, :] = pdf_unrolled
+
+    kernel = fill_rows(sparse_prob)
+    if output_file is not None:
+        np.savez(output_file, kernel=kernel, init_min=init_min, init_max=init_max)
+    return kernel
+
+
+def load_dummy(pkl_path):
+    pkl = np.load(pkl_path, allow_pickle=True)
+    sequence = pkl["full_series"]
+    # Back to floats
+    sequence = np.array([int(num) for num in sequence.split(",")[:-1]])
+    sequence = sequence[:100]
+    return {"full_series": sequence}
+
+
+def process_pdf(args, llama, good_tokens, param_tuple):
+    param_name, param_seq = param_tuple
+    pdf_path = os.path.join(args.output_dir, "pdfs", f"{param_name}.pkl")
+    if args.load and os.path.exists(pdf_path):
+        with open(pdf_path, "rb") as pdf_file:
+            pdfs = pickle.load(pdf_file)["pdf"]
+    else:
+        pdfs = get_pdf(param_seq, llama, good_tokens, output_file=pdf_path)
+    return param_name, {
+        "pdf": pdfs,
+        "states": param_seq,
+        "init_min": sequences[param_name].min(),
+        "init_max": sequences[param_name].max(),
+    }
+
+
+def process_kernel(args, param_tuple):
+    param_name, param_dict = param_tuple
+    output_file = os.path.join(args.output_dir, "kernel", f"{param_name}.npz")
+    kernel = get_kernel(
+        param_dict["pdf"],
+        param_dict["states"],
+        param_dict["init_min"],
+        param_dict["init_max"],
+        output_file=output_file,
+    )
+    return param_name, kernel
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ckpts_path", type=str, required=False, help="Path to directory containing the checkpoints")
+    parser.add_argument("--llama_v", choices=[2, 3], type=int, required=True, help="Version of Llama model")
+    parser.add_argument(
+        "--output_dir", type=str, required=True, help="Path to directory to save inferred PDFs and kernels"
+    )
+    parser.add_argument("--load", action="store_true", help="if true, load PDFs from output_dir if possible")
+    parser.add_argument("--use-dummy", dest="use_dummy", action="store_true", help="Use dummy data for testing")
+    parser.add_argument("--n_threads", type=int, default=mp.cpu_count(), help="Number of threads for parallel processing")
+    args = parser.parse_args()
+
+    llama = Llama(llama_v=args.llama_v)
+
+    if args.use_dummy:
+        sequences = load_dummy("tmp/brownian_motion_0.pkl")
+        rescaled_sequences = sequences
+    else:
+        sequences = load_ckpts_into_seq(args.ckpts_path)
+        rescaled_sequences = {
+            param_name: llama.tokenizer.rescale(param_seq) for param_name, param_seq in sequences.items()
+        }
+
+    good_tokens_str = list("0123456789")
+    good_tokens = [llama.llama_tokenizer.convert_tokens_to_ids(token) for token in good_tokens_str]
+
+    os.makedirs(os.path.join(args.output_dir, "pdfs"), exist_ok=True)
+    with mp.Pool(args.n_threads) as pool:
+        process_pdf_partial = partial(process_pdf, args, llama, good_tokens)
+        pdf_results = pool.map(process_pdf_partial, rescaled_sequences.items())
+    pdf_dict = dict(pdf_results)
+
+    os.makedirs(os.path.join(args.output_dir, "kernel"), exist_ok=True)
+    with mp.Pool(args.n_threads) as pool:
+        process_kernel_partial = partial(process_kernel, args)
+        kernel_results = pool.map(process_kernel_partial, pdf_dict.items())
+    kernels_dict = dict(kernel_results)
